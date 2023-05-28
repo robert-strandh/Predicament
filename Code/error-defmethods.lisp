000f84ca96ad60ca519303e920f32033d29d8c86
Robert Strandh <robert.strandh@gmail.com>
2023-05-28 10:21:11 +0200

    Bug fix, call ERROR recursively rather than using CALL-NEXT-METHOD.

--- Code/error-defmethods.lisp
+++ Code/error-defmethods.lisp
@@ -6,16 +6,14 @@
   (invoke-debugger datum))
 
 (defmethod error :around ((datum string) &rest arguments)
-  (call-next-method
-   (make-condition 'simple-error
-                   :format-control datum
-                   :format-arguments arguments)
-   nil))
+  (error (make-condition 'simple-error
+                         :format-control datum
+                         :format-arguments arguments)
+         nil))
 
 (defmethod error :around ((datum symbol) &rest arguments)
-  (call-next-method
-   (apply #'make-condition datum arguments)
-   nil))
+  (error (apply #'make-condition datum arguments)
+         nil))
 
 (defmethod error :before ((datum condition) &rest arguments)
   (unless (null arguments)
