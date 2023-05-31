7e2632e15a095359f81667fcffc8d82c98f228c8
Robert Strandh <robert.strandh@gmail.com>
2023-05-31 09:13:07 +0200

    Change the name of this parameter and the structure of its value.

--- Code/active-restarts-defparameter.lisp
+++ Code/active-restarts-defparameter.lisp
@@ -1,11 +1,17 @@
 (cl:in-package #:predicament)
 
-(defparameter *active-restarts* '())
+(defparameter *restart-clusters* '())
 
-(setf (documentation '*active-restarts* 'variable)
+(setf (documentation '*restart-clusters* 'variable)
       (format nil
               "Initial value: () ; the empty list~@
                ~@
-               The value of this variable is a proper list of all~@
-               active restarts, ordered from the most recently~@
-               established, to the least recently established."))
+               The value of this variable is a proper list of~@
+               \"restart clusters\", where a restart cluster is~@
+               a proper list of restarts established by a single~@
+               form such as RESTART-CASE or RESTART-BIND.  Together~@
+               the restarts in all the clusters are the \"active\"~@
+               restarts.  The clusters in this list are orderd from~@
+               most recently establshed to least recently established,~@
+               and the restarts in a cluster are also ordered the same~@
+               way."))
