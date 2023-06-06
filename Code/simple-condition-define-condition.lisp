ebadf3e53fd17dadb30d759b38825176f08f5520
Robert Strandh <robert.strandh@gmail.com>
2023-06-06 12:19:49 +0200

    Provide documentation for SIMPLE-CONDITION-FORMAT-CONTROL/ARGUMENTS.

--- Code/simple-condition-define-condition.lisp
+++ Code/simple-condition-define-condition.lisp
@@ -32,3 +32,19 @@
                using the :FORMAT-ARGUMENTS initarg, and this information~@
                can be read from the condition instance by using the~@
                function SIMPLE-CONDITION-FORMAT-ARGUMENTS."))
+
+(setf (documentation 'simple-condition-format-control 'function)
+      (format nil
+              "Syntax simple-condition-format-control simple-condition~@
+               ~@
+               This function returns the information supplied by the~@
+               initarg :FORMAT-CONTROL to MAKE-CONDITION when the condition~@
+               type to be instantiated is (a subclass of) SIMPLE-CONDITION."))
+
+(setf (documentation 'simple-condition-format-arguments 'function)
+      (format nil
+              "Syntax simple-condition-format-arguments simple-condition~@
+               ~@
+               This function returns the information supplied by the~@
+               initarg :FORMAT-ARGUMENTS to MAKE-CONDITION when the condition~@
+               type to be instantiated is (a subclass of) SIMPLE-CONDITION."))
