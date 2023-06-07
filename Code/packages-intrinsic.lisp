9919243e5d12a5f65c4f62d9f0e17ccbdea6c12c
Robert Strandh <robert.strandh@gmail.com>
2023-06-07 16:11:46 +0200

    Define package-local nicknames for Iconoclast etc.

--- Code/packages-intrinsic.lisp
+++ Code/packages-intrinsic.lisp
@@ -2,4 +2,7 @@
 
 (defpackage predicament
   (:use #:common-lisp)
+  (:local-nicknames (#:ico #:iconoclast)
+                    (#:bld #:iconoclast-builder)
+                    (#:ses #:s-expression-syntax))
   (:export . #.predicament-asdf:*string-designators*))
