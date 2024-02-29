c386763e59c46efa6ca778a60aa31cf6d36ea384
Robert Strandh <robert.strandh@gmail.com>
2024-02-29 13:31:33 +0100

    Rewrite macros.

--- Code/restart-bind-defmacro.lisp
+++ Code/restart-bind-defmacro.lisp
@@ -1,32 +1,10 @@
 (cl:in-package #:predicament)
 
-(defmacro restart-bind (&whole form bindings &body forms)
-  (declare (ignore bindings forms))
-  (let* ((builder (make-instance 'bld:builder))
-         (syntax (ses:find-syntax 'restart-bind))
-         (ast (ses:parse builder syntax form)))
-    (let ((binding-asts (ico:binding-asts ast)))
-      (flet ((make-make-instance-form (binding-ast)
-               `(make-instance 'restart
-                  :name ,(ico:name (ico:name-ast ast))
-                  :action ,(ico:form (ico:function-form-ast binding-ast))
-                  :interactive-function
-                  ,(if (null (ico:interactive-function-ast ast))
-                       'nil
-                       (ico:form (ico:interactive-function-ast binding-ast)))
-                  :report-function
-                  ,(if (null (ico:report-function-ast ast))
-                       'nil
-                       (ico:form (ico:report-function-ast binding-ast)))
-                  :test-function
-                  ,(if (null (ico:test-function-ast ast))
-                       'nil
-                       (ico:form (ico:test-function-ast binding-ast))))))
-        `(let ((*restart-clusters*
-                 (cons (list ,@(loop for binding-ast in binding-asts
-                                     collect (make-make-instance-form
-                                              binding-ast)))
-                       *restart-clusters*)))
-           ,@(loop for form in (ico:form (ico:form-asts ast))
-                   collect form))))))
+(defun restart-bind-transform-binding (binding)
+  (destructuring-bind (name function . arguments) binding
+    `(make-restart :name ',name :function ,function ,@arguments)))
 
+(defmacro restart-bind (bindings &body body)
+  (let ((cluster (mapcar #'restart-bind-transform-binding bindings)))
+    `(let ((*restart-clusters* (cons (list ,@cluster) *restart-clusters*)))
+       ,@body)))
--- Code/restart-case-defmacro.lisp
+++ Code/restart-case-defmacro.lisp
@@ -1,73 +1,116 @@
 (cl:in-package #:predicament)
 
-(defun transform-restart-case-options (clause-ast)
-  (with-accessors ((interactive-ast ico:interactive-ast)
-                   (report-ast ico:report-ast)
-                   (test-ast ico:test-ast))
-      clause-ast
-    `(,@(if (null interactive-ast)
-            '()
-            `(:interactive
-              (function 
-               ,(if (typep interactive-ast 'ico:function-name-ast)
-                    (ico:name interactive-ast)
-                    (ico:form (ico:form-ast interactive-ast))))))
-      ,@(if (null report-ast)
-            '()
-            `(:report
-              (function 
-               ,(cond ((typep report-ast 'ico:unparsed-form-ast)
-                       `(lambda (stream)
-                          (write-string ,(ico:form report-ast))))
-                      ((typep report-ast 'ico:function-name-ast)
-                       (ico:name report-ast))
-                      (t
-                       (ico:form (ico:form-ast report-ast)))))))
-      ,@(if (null test-ast)
-            '()
-            `(:test
-              (function 
-               ,(if (typep test-ast 'ico:function-name-ast)
-                    (ico:name test-ast)
-                    (ico:form (ico:form-ast test-ast)))))))))
+(defun restart-case-make-report-subform (report)
+  (typecase report
+    (null '())
+    (string `(:report-function (lambda (stream) (write-string ,report stream))))
+    (t (list :report-function (list 'function report)))))
 
-;;; FIXME: handle restarts associated with a condition.
-(defmacro restart-case (&whole form restartable-form &rest clauses)
-  (declare (ignore restartable-form clauses))
-  (let* ((builder (make-instance 'bld:builder))
-         (syntax (ses:find-syntax 'cl:restart-case))
-         (modified-form (cons 'cl:restart-case (cdr form)))
-         (ast (ses:parse builder syntax modified-form))
-         (clause-asts (ico:clause-asts ast)))
-    (let ((block-name (gensym))
-          (variable-name (gensym))
-          (tagbody-tags (loop for clause-ast in clause-asts
-                              collect (gensym))))
-      `(block ,block-name
-         (let ((,variable-name nil))
+(defun restart-case-make-interactive-subform (interactive)
+  (typecase interactive
+    (null '())
+    (t (list :interactive-function (list 'function interactive)))))
+
+(defun restart-case-make-test-subform (test)
+  (typecase test
+    (null '())
+    (t (list :test-function (list 'function test)))))
+
+(defun restart-case-pop-keywords-from-case (case-forms)
+  (let ((things case-forms) report interactive test)
+    (macrolet ((handle-keyword (symbol keyword)
+                 (let ((value (gensym "KEYWORD-VALUE")))
+                   `(progn
+                      (when ,symbol
+                        (error "Duplicate ~S in case ~S." ,keyword case-forms))
+                      (pop things)
+                      (let ((,value (pop things)))
+                        (unless ,value
+                          (error "~S may not be NIL in HANDLER-CASE." ,keyword))
+                        (setf ,symbol ,value))))))
+      (loop
+        (let ((thing (first things)))
+          (cond
+            ((null (rest things))
+             (return (values things report interactive test)))
+            ((eq thing :report) (handle-keyword report :report))
+            ((eq thing :interactive) (handle-keyword interactive :interactive))
+            ((eq thing :test) (handle-keyword test :test))
+            (t (return (values things report interactive test)))))))))
+
+(defun restart-case-expand-non-cerror (expansion)
+  (destructuring-bind (function datum . args) expansion
+    (let* ((type (case function
+                   (signal 'simple-condition)
+                   (warn 'simple-warning)
+                   (error 'simple-error)))
+           (condition (gensym "CONDITION")))
+      `(let ((,condition (coerce-to-condition ,datum (list ,@args)
+                                              ',type ',function)))
+         (with-condition-restarts ,condition (car *restart-clusters*)
+           (,function ,condition))))))
+
+(defun restart-case-expand-cerror (expansion)
+  (destructuring-bind (function format-control datum . args) expansion
+    (let* ((type 'simple-error)
+           (condition (gensym "CONDITION")))
+      `(let ((,condition (coerce-to-condition ,datum (list ,@args)
+                                              ',type ',function)))
+         (with-condition-restarts ,condition (car *restart-clusters*)
+           (,function ,format-control ,condition))))))
+
+;;; RESTART-CASE - bindings and cases
+
+(defun restart-case-make-restart-binding (temp-var parsed-case)
+  (destructuring-bind (name lambda-list keys body tag) parsed-case
+    (declare (ignore lambda-list body))
+    (let ((lambda-var (gensym "RESTART-ARGS")))
+      `(,name
+        (lambda (&rest ,lambda-var) (setq ,temp-var ,lambda-var) (go ,tag))
+        ,@keys))))
+
+(defun restart-case-make-restart-case (block-tag temp-var parsed-case)
+  (destructuring-bind (name lambda-list keys body tag) parsed-case
+    (declare (ignore name keys))
+    `(,tag
+      (return-from ,block-tag (apply (lambda ,lambda-list ,@body) ,temp-var)))))
+
+(defun restart-case-signaling-form-p (expression env)
+  (let ((expansion (macroexpand expression env)))
+    (and (consp expansion)
+         (member (car expansion) '(signal warn error cerror)))))
+
+(defun restart-case-expand-signaling-form (expression env)
+  (let ((expansion (macroexpand expression env)))
+    (case (car expansion)
+      ((signal warn error) (restart-case-expand-non-cerror expansion))
+      (cerror (restart-case-expand-cerror expansion)))))
+
+(defun restart-case-parse-case (case)
+  (destructuring-bind (name lambda-list . rest) case
+    (multiple-value-bind (body report interactive test)
+        (restart-case-pop-keywords-from-case rest)
+      (let ((tag (gensym))
+            (keywords `(,@(restart-case-make-report-subform report)
+                        ,@(restart-case-make-test-subform test)
+                        ,@(restart-case-make-interactive-subform interactive))))
+        (list name lambda-list keywords body tag)))))
+
+(defmacro restart-case (expression &body cases &environment env)
+  (let ((block-name (gensym "RESTART-CASE-BLOCK"))
+        (temp-var (gensym "RESTART-CASE-VAR"))
+        (data (mapcar #'restart-case-parse-case cases)))
+    (flet ((make-restart-binding (datum)
+             (restart-case-make-restart-binding temp-var datum))
+           (make-restart-case (datum)
+             (restart-case-make-restart-case block-name temp-var datum)))
+      `(let ((,temp-var nil))
+         (declare (ignorable ,temp-var))
+         (block ,block-name
            (tagbody
-              (restart-bind
-                  ,(loop for clause-ast in clause-asts
-                         for tagbody-tag in tagbody-tags
-                         collect
-                         `((,(ico:name (ico:name-ast clause-ast))
-                            (lambda (&rest temp)
-                              (setq ,variable-name temp)
-                              (go ,tagbody-tag)))
-                           ,@(transform-restart-case-options clause-ast)))
-                (return-from ,block-name ,(ico:form (ico:form-ast ast))))
-              ,@(loop for clause-ast in clause-asts
-                      for tagbody-tag in tagbody-tags
-                      collect tagbody-tag
-                      collect `(return-from ,block-name
-                                 (apply (lambda
-                                          ,(ico:unparse-lambda-list-ast
-                                            (ico:lambda-list-ast ast))
-                                          ;; FIXME: handle declarations
-                                          ,@(loop for form-ast
-                                                    in (ico:form-asts ast)
-                                                  collect
-                                                  (ico:form form-ast)))
-                                        ,variable-name)))))))))
-                                        
-        
+              (restart-bind ,(mapcar #'make-restart-binding data)
+                (return-from ,block-name
+                  ,(if (restart-case-signaling-form-p expression env)
+                       (restart-case-expand-signaling-form expression env)
+                       expression)))
+              ,@(apply #'append (mapcar #'make-restart-case data))))))))
