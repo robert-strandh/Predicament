6cc5b0ef5d7a0124f1b2b1c44e9a3f02461051fa
Robert Strandh <robert.strandh@gmail.com>
2024-03-05 13:43:38 +0100

    Supply documentation string for COMPUTE-RESTARTS.

--- Code/compute-restarts.lisp
+++ Code/compute-restarts.lisp
@@ -16,3 +16,22 @@
   (loop for restart in (apply #'append *restart-clusters*)
         when (restart-visible-p restart condition)
           collect restart))
+
+(setf (documentation 'compute-restarts 'function)
+      (format nil
+              "Syntax: compute-restarts &optional condition~@
+               ~@
+               This function returns a list of all the restarts~@
+               that are currently active.  The list is ordered from~@
+               most recently established restart to least recently~@
+               established restart.
+               ~@
+               When the optional argument CONDITION is non-NIL, then~@
+               then a restart is included in the resulting list only~@
+               if it is either explicitly associated with CONDITION~@
+               or if it is not associated with any condition at all.~@
+               If CONDITION is not given, or its value is NIL, then~@
+               all restarts are considered.~@
+               ~@
+               As permitted by the Common Lisp standard, this function~@
+               returns a fresh list for each invocation."))
