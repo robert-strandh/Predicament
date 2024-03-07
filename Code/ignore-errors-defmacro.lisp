9cbf38a038a484e8a02ee303de984dca16616bfd
Robert Strandh <robert.strandh@gmail.com>
2024-03-07 11:14:14 +0100

    Provide documentation for IGNORE-ERRORS.

--- Code/ignore-errors-defmacro.lisp
+++ Code/ignore-errors-defmacro.lisp
@@ -3,3 +3,15 @@
 (defmacro ignore-errors (&rest forms)
   `(handler-case (progn ,@forms)
      (error (condition (values nil condition)))))
+
+(setf (documentation 'ignore-errors 'function)
+      (format nil
+              "Syntax: ignore-errors form*~@
+               ~@
+               This macro establishes a handler for the condition~@
+               type ERROR, and then evaluates FORM* from left to right~@
+               (implicit PROGN).  If the evaluation of FORM* does not~@
+               signal an ERROR, then this macro returns the values~@
+               returned by FORM*.  If the evaluation of FORM* results~@
+               in an ERROR being signaled, this macro return two values,~@
+               namely NIL and the condition that was signaled."))
