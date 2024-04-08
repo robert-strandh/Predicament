c914d32ca08ac378e7a3c2af0e59f30c2692a083
Robert Strandh <robert.strandh@gmail.com>
2024-04-08 15:12:02 +0200

    Use uninterned symbol for package name.

--- Code/packages-extrinsic.lisp
+++ Code/packages-extrinsic.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:common-lisp-user)
 
-(defpackage predicament
+(defpackage #:predicament
   (:use #:common-lisp)
   (:shadow . #.predicament-asdf:*string-designators*)
   (:export . #.predicament-asdf:*string-designators*))
