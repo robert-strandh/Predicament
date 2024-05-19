041ab801f04d43090fe85411519b6b062a7ce918
Robert Strandh <robert.strandh@gmail.com>
2024-05-19 05:36:20 +0200

    Remove redundant package prefix.

--- Code/cerror-defun.lisp
+++ Code/cerror-defun.lisp
@@ -2,4 +2,4 @@
 
 (defun cerror (continue-format-control datum &rest arguments)
   (with-simple-restart (continue continue-format-control arguments)
-    (apply #'predicament:error datum arguments)))
+    (apply #'error datum arguments)))
