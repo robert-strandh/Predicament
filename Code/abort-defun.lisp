305c1df780c162828980f61dd17a3f87a7f87a6c
Robert Strandh <robert.strandh@gmail.com>
2024-05-19 08:29:30 +0200

    Provide documentation string for ABORT..

--- Code/abort-defun.lisp
+++ Code/abort-defun.lisp
@@ -5,3 +5,19 @@
     (if (null condition)
         (error 'control-error)
         (invoke-restart restart))))
+
+(setf (documentation 'abort 'function)
+      (format nil
+              "Syntax: abort &optional condition~@
+               ~@
+               This function transfers control to the most recently~@
+               established applicable ABORT restart.  If no such restart~@
+               exists, signals an error of type CONTROL-ERROR.~@
+               ~@
+               CONDITION is a condition object or NIL.  If condition is~@
+               a condition object, then only those restarts are considered~@
+               that are explicitly associated with CONDITION, or not~@
+               associated with any condition at all.  If CONDITION is NIL,~@
+               then all established applicable ABORT restarts are~@
+               considered.  If CONDITION is neither a condition object~@
+               nor NIL, an error of type TYPE-ERROR is signaled."))
