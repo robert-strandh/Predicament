1d6829a153c85e22bfe6edb598a84dcce9ee96b4
Robert Strandh <robert.strandh@gmail.com>
2024-05-19 08:48:32 +0200

    Provide documentation string for RESTART-NAME.

--- Code/restart-defclass.lisp
+++ Code/restart-defclass.lisp
@@ -21,3 +21,14 @@
     :initform (constantly t)
     :initarg :test-function
     :reader test-function)))
+
+(setf (documentation 'restart-name 'function)
+      (format nil
+              "Syntax: restart-name restart~@
+               ~@
+               This function returns the name of the restart RESTART,~@
+               or NIL if the restart has no name.~@
+               ~@
+               If RESTART is not a restart object, then an error of type~@
+               TYPE-ERROR is signaled."))
+               
