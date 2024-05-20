5df0d5d5c7acb410eaf2c97b88c2d282b7c75746
Robert Strandh <robert.strandh@gmail.com>
2024-05-20 10:11:05 +0200

    Provide documentation for BREAK.

--- Code/break-defun.lisp
+++ Code/break-defun.lisp
@@ -6,3 +6,18 @@
       (invoke-debugger (make-condition 'simple-condition
                                        :format-control format-control
                                        :format-arguments format-arguments)))))
+
+(setf (documentation 'break 'function)
+      (format nil
+              "Syntax: break &optional format-control &rest arguments~@
+               ~@
+               This function enters the debugger by creating an instance~@
+               of SIMPLE-CONDITION using FORMAT-CONTROL as a format control~@
+               and FORMAT-ARGUMENTS as format arguments.  The default~@
+               value of FORMAT-CONTROL is the string \"Break\".~@
+               ~@
+               If the CONTINUE restart is invoked while the debugger~@
+               is invoked, then this function returns NIL.~@
+               ~@
+               Before entering the debugger, this function binds the~@
+               variable *DEBUGGER-HOOK* to NIL."))
